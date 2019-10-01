7f0b53d31913d93b022125f8f029f2ee7d5920e4
Hanul <hanul@hanul.me>
2019-10-01 20:48:11 +0900

    계정 생성 기능 추가

--- DPlayCreateAccount/BROWSER/Home.js
+++ DPlayCreateAccount/BROWSER/Home.js
@@ -6,8 +6,67 @@ DPlayCreateAccount.Home = CLASS({
 
 	init : (inner, self) => {
 		
+		let mnemonic = bip39.generateMnemonic();
+		
+		let seed = bip39.mnemonicToSeed(mnemonic);
+		
+		let rootKey = ethereumjs.WalletHD.fromMasterSeed(seed);
+		let hardenedKey = rootKey.derivePath('m/44\'/60\'/0\'/0');
+		let childKey = hardenedKey.deriveChild(0);
+		
+		let wallet = childKey.getWallet();
+		
+		TITLE(MSG('TITLE'));
+		
 		let wrapper = DIV({
-			c : 'Hello!'
+			c : [
+			H1({
+				c : MSG('TITLE')
+			}),
+			
+			P({
+				c : MSG('DESCRIPTION')
+			}),
+			
+			DIV({
+				c : [
+				H2({
+					c : MSG('CREATED_ACCOUNT_ID')
+				}),
+				P({
+					c : wallet.getChecksumAddressString()
+				})]
+			}),
+			
+			DIV({
+				c : [
+				H2({
+					c : MSG('MNEMONIC')
+				}),
+				P({
+					c : mnemonic
+				}),
+				P({
+					c : MSG('BACKUP_NOTICE_1') + '\n\n' + MSG('BACKUP_NOTICE_2')
+				})]
+			}),
+			
+			UUI.V_CENTER({
+				style : {
+					marginTop : 10,
+					width : 326,
+					height : 66,
+					backgroundImage : '/DPlayCreateAccount/R/button.png',
+					cursor : 'pointer',
+					textAlign : 'center'
+				},
+				c : MSG('REGENERATE_ACCOUNT_BUTTON'),
+				on : {
+					tap : () => {
+						REFRESH();
+					}
+				}
+			})]
 		}).appendTo(BODY);
 		
 		inner.on('close', () => {
